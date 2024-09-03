5e5b37d8cddeb6feb770b8c3622efc2f99d1f91f
Abnt140 <abnertadeu1409@gmail.com>
2024-09-03 09:43:06 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,17 @@ function openSidebar() {
 function closeSidebar() {
     document.getElementById("mySidebar").style.width = "0";
 }
+document.addEventListener("DOMContentLoaded", function() {
+    var acc = document.getElementsByClassName("accordion");
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+            } 
+        });
+    }
+});
